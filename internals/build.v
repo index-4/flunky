27cc1645b47aa5cc3f964e7f7cd674a277111e24
liquidiert <haberederkorbinian@googlemail.com>
2021-04-17 14:29:14 +0200

    feature null support :sparkles:
      - further null support for class args
      - fixed normal flutter run

--- internals/build.v
+++ internals/build.v
@@ -16,7 +16,7 @@ fn generate_router_define_str(route Route) []string {
 	if route.string_param != "" {
 		handler = 'return ${route.handler_recv}(params["${route.string_param}"]![0]);'
 	} else if route.class_param != "" {
-		handler = "return ${route.handler_recv}(context!.settings!.arguments as ${route.class_param});"
+		handler = "return ${route.handler_recv}(${route.class_param}.fromNullable(context!.settings!.arguments));"
 	} else {
 		handler = "return ${route.handler_recv}();"
 	}
